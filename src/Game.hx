2689d5fa833f9f12b7eafd7aa84e52903ada5080
Karlo <Karlo@localhost>
2013-01-25 12:07:09 +0800

    Can now show an score indicator on the screen

--- src/Game.hx
+++ src/Game.hx
@@ -1,8 +1,10 @@
 package ;
 import jkEngine.JKGame;
 import jkEngine.JKSprite;
+import jkEngine.JKText;
 import nme.events.Event;
 import nme.Lib;
+import nme.text.TextFormatAlign;
 /**
  * ...
  * @author Karlo
@@ -11,15 +13,17 @@ import nme.Lib;
 class Game extends JKGame
 {	
 	public var playArea : PlayArea;	
-	public var isGameOver : Bool = false;
+	public var isGameOver : Bool = false;	
 	var countdownTimer : CountdownTimer;
 	var guiArea : JKSprite;	
-	
+	var scoreView : JKText;
+	var scoreCount : Int;	
 	
 	public function new() 
 	{
 		Registry.game = this;
-		guiArea = new JKSprite();				
+		guiArea = new JKSprite();
+		scoreView = new JKText(260, 20, 100, "0", 0xFFFFFF, 50, TextFormatAlign.RIGHT, "Arial", guiArea);
 		countdownTimer = new CountdownTimer(40, 20, guiArea);
 		
 		super();		
